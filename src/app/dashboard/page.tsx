a3b57714694f846f9baf9abb4c63bee967544707
AliSohani <a.sohani.shahrood@gmail.com>
2023-04-23 11:04:46 +0330

    boiler plate and added Button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@/components/ui/Button";
+
+interface pageProps {}
+
+const page = (props: pageProps) => {
+  return <Button>hello</Button>;
+};
+
+export default page;
